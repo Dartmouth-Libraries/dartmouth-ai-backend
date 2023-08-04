f4176210b7b8bbe126f48861d6cb44aa315f0a42
Simon Stone <simon.stone@dartmouth.edu>
2023-08-04 13:47:18 -0400

    fixed test

--- test/test.py
+++ test/test.py
@@ -5,11 +5,6 @@ from dartmouth_ai_backend.named_entity_recognition import NamedEntityRecognizer
 import json
 
 
-def test_object_detection():
-    result = ObjectDetector().detect("test/object_detection_sample.jpg")
-    assert result
-
-
 def test_language_detection():
     with open("test/de.txt") as f:
         de_text = f.read()
@@ -33,6 +28,13 @@ def test_named_entity_recognition():
     assert result
 
 
+def test_object_detection():
+    result = ObjectDetector().detect("test/object_detection_sample.jpg")
+    print(result)
+    assert result
+
+
 if __name__ == "__main__":
     test_language_detection()
     test_named_entity_recognition()
+    test_object_detection()
