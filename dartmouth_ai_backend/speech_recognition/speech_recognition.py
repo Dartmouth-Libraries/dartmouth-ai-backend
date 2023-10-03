092e93dc49fe9c1d84b839ea47c14625c637a803
Simon Stone <simon.stone@dartmouth.edu>
2023-10-03 19:23:07 -0400

    merge

--- dartmouth_ai_backend/speech_recognition/speech_recognition.py
+++ dartmouth_ai_backend/speech_recognition/speech_recognition.py
@@ -1,10 +1,13 @@
 import whisper
+import librosa
+
 
 class SpeechRecognizer:
     def __init__(self, model="large-v2"):
         self.__model = whisper.load_model(model)
 
     def transcribe(self, speech_file, task="transcribe", language=None) -> str:
-        transcription = self.__model.transcribe(speech_file, task=task)
+        waveform, sample_rate = librosa.load(speech_file, mono=True, sr=16000)
+        transcription = self.__model.transcribe(waveform, task=task, language=language)
 
         return transcription
