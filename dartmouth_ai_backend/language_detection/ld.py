7c9b912f4549fe5a135de25376c9f6498d579e80
Simon Stone <simon.stone@dartmouth.edu>
2023-10-09 16:57:22 -0400

    removes a redundant parameter

--- dartmouth_ai_backend/language_detection/ld.py
+++ dartmouth_ai_backend/language_detection/ld.py
@@ -4,20 +4,15 @@ import spacy_fastlang  # Ignore warning about unused import!
 
 
 class LanguageDetector:
-    def __init__(self, lang='en'):
-        if lang != 'en':
-            raise NotImplementedError("Language Detection is currently "
-                                      "only supported for English texts.")
+    """Detects the language of a text"""
 
+    def __init__(self):
         self.__nlp = spacy.load("en_core_web_trf")
         self.__nlp.add_pipe("language_detector")
 
     def detect(self, text):
         detected = self.__nlp(text)
-        return {
-            "language": detected._.language,
-            "score": detected._.language_score
-        }
+        return {"language": detected._.language, "score": detected._.language_score}
 
 
 if __name__ == "__main__":
