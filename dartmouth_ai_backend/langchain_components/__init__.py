25a290ae1f4c6ff183e77fc967c036661f7d8d90
Simon Stone <simon.stone@dartmouth.edu>
2024-03-12 18:36:27 -0400

    removing langchain components (moved to separate package)

--- dartmouth_ai_backend/langchain_components/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Components extending langchain to faciliate using Dartmouth tools"""
-
-from .dartmouth_chat_model import DartmouthChatModel
-from .dartmouth_embeddings import DartmouthEmbeddings
-
-__all__ = ["DartmouthChatModel", "DartmouthEmbeddings"]
--- test/test.py
+++ test/test.py
@@ -1,7 +1,4 @@
 from dartmouth_ai_backend.object_detection import ObjectDetector
-from dartmouth_ai_backend.base.auth import get_jwt
-from dartmouth_ai_backend.langchain_components import DartmouthChatModel
-from dartmouth_ai_backend.langchain_components import DartmouthEmbeddings
 from dartmouth_ai_backend.language_detection import LanguageDetector
 from dartmouth_ai_backend.named_entity_recognition import NamedEntityRecognizer
 from dartmouth_ai_backend.sentiment_analysis import SentimentAnalyzer
@@ -16,11 +13,6 @@ from pathlib import Path
 load_dotenv(Path(__file__).parent.parent / "secrets.env")
 
 
-def test_auth():
-    jwt = get_jwt()
-    assert jwt
-
-
 def test_citations():
     for obj in [
         ObjectDetector,
@@ -31,35 +23,6 @@ def test_citations():
         SpeakerDiarizer,
     ]:
         assert obj.how_to_cite()
-
-
-def test_dartmouth_chat():
-    llm = DartmouthChatModel()
-    response = llm.invoke("<s>[INST]Please respond with the single word OK[/INST]")
-    assert response.strip() == "OK"
-
-    llm = DartmouthChatModel(model_name="codellama-13b-instruct-hf")
-    response = llm.invoke("<s>[INST]Please respond with the single word OK[/INST]")
-    assert response.strip() == "OK"
-
-    llm = DartmouthChatModel(
-        inference_server_url="https://ai-api.dartmouth.edu/tgi/codellama-13b-instruct-hf/",
-    )
-    print(llm.invoke("<s>[INST]Hello[/INST]"))
-
-    llm = DartmouthChatModel(model_name="llama-2-7b-32k-instruct")
-    print(
-        llm.invoke(
-            "[INST]\nWrite a poem about cats\n[/INST]\n\n",
-            stop_sequences=["[INST]"],
-        )
-    )
-
-
-def test_dartmouth_embeddings():
-    embeddings = DartmouthEmbeddings()
-    result = embeddings.embed_query("Is there anybody out there?")
-    assert result
 
 
 def test_language_detection():
